ba1e71da4ea8960cb53ff3f007e857691fc8eb0b
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-08-07 10:38:39 -0400

    checkpoint before modifying

--- init.lua
+++ init.lua
@@ -202,6 +202,7 @@ require('lazy').setup({
         },
         --Keymaps start with 'r' for 'REPL'
         keymaps = {
+          --Keymaps start with 'r' for 'REPL'
           send_motion = '<space>rc',
           visual_send = '<space>rc',
           send_file = '<space>rf',
@@ -958,7 +959,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
