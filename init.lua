6bbbf452cfa67f4a21d6c53c70e494a888aa126d
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-09-09 11:21:53 -0400

    section comment color added

--- init.lua
+++ init.lua
@@ -911,7 +911,37 @@ require('lazy').setup({
     end,
   },
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = true,
+      keywords = {
+        --FIX:
+        FIX = {
+          icon = ' ', -- icon used for the sign, and in search results
+          color = 'error', -- can be a hex color, or a named color (see below)
+          alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+          -- signs = false, -- configure signs for some keywords individually
+        },
+        --TODO:
+        TODO = { icon = ' ', color = 'info' },
+        --HACK:
+        HACK = { icon = ' ', color = 'warning' },
+        --WARN:
+        WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+        --PERF:
+        PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+        --NOTE:
+        NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+        --TEST:
+        TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+        --SECTION:
+        SECTION = { icon = '󰚟', color = 'hint', alt = { '#' } },
+      },
+    },
+  },
 
   { -- collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
